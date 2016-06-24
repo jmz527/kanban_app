8ebfc32412bd469369ef2dae5b30b64d7713eba9
James Rutledge <jarutledge2@gmail.com>
2016-06-24 16:28:08 -0400

    babel included reluctantly

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// require('./main.css');
+
+// var component = require('./component');
+// var app = document.createElement('div');
+
+// document.body.appendChild(app);
+
+// app.appendChild(component());
